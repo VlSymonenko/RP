59a1e90a4b22beb40224092e1fb4c72a71ac4bd9
VlSymonenko <symonenkovlad13@gmail.com>
2019-04-07 20:36:11 +0300

    :)

--- src/index.js
+++ src/index.js
@@ -8,23 +8,63 @@ class App extends React.Component {
   constructor (props) {
     super(props);
     this.state = {
-      email: ''
+        formControls: {
+            email: {
+              value: ''
+            },
+            name: {
+              value: ''
+            },
+            password: {
+              value: ''
+            }
+        }
     }
-  }
   
-  changeHandler = event => {
-    this.setState({
-      email: event.target.value
-    });
-  }
+}
 
-  render () {
+changeHandler = event => {
+    
+    const name = event.target.name;
+    const value = event.target.value;
+  
+    this.setState({
+      formControls: {
+          ...this.state.formControls,
+          [name]: {
+          ...this.state.formControls[name],
+          value
+        }
+      }
+    });
+}
+
+
+ render() {
     return (
-      <form>
-          <input type="email"  name="email" value={this.state.email} onChange={this.changeHandler} />
-      </form>
-    );        
-  }
+        <form>
+
+            <input type="email" 
+                   name="email" 
+                   value={this.state.formControls.email.value} 
+                   onChange={this.changeHandler} 
+            />
+
+            <input type="text" 
+                   name="name" 
+                   value={this.state.formControls.name.value} 
+                   onChange={this.changeHandler} 
+            />
+
+            <input type="password" 
+                   name="password" 
+                   value={this.state.formControls.password.value} 
+                   onChange={this.changeHandler} 
+            />
+
+        </form>      
+    );
+ }
 }
 
 ReactDOM.render(<App />, document.getElementById('app'));
