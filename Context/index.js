a85cb01d1df0c4d0d201608131b88efab2f96b29
VlSymonenko <symonenkovlad13@gmail.com>
2019-04-09 11:02:30 +0300

    Add routing and Context

---
+++ Context/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { FamilyProvider, FamilyConsumer } from "./FamilyContext";
+
+export class Grandmother extends React.Component {
+  state = {
+    lastName: "Sanchez"
+  };
+
+  render() {
+    return (
+      // We wrap all of the components that need access 
+      // to the lastName property in FamilyProvider.
+      <FamilyProvider value={this.state.lastName}>
+        <Mother />
+      </FamilyProvider>
+    );
+  }
+}
+
+const Mother = () => {
+  return <Child />;
+};
+
+const Child = () => {
+  // We wrap the component that actaully needs access to 
+  // the lastName property in FamilyConsumer
+  return <FamilyConsumer>{context => <p>{context}</p>}</FamilyConsumer>;
+};
